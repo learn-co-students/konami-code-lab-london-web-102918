11d5612b82e5b774ff8fc013fd6c03251b1f99ec
Paul-Cl-ark <clark.paul123@gmail.com>
2018-12-12 14:27:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
-function init() {
+function init () {
   // your code here
+  let ind = 0
+  document.addEventListener('keydown', (event) => {
+    const key = event.key
+    // ind = (codes[ind] === key) ? ++ind : 0
+    if (codes[ind] === key) {
+      ++ind
+    } else {
+      ind = 0
+    }
+    if (ind === codes.length) {
+      window.alert('Easter egg!')
+      ind = 0
+    }
+  })
 }

83e7c8418474198f1df0baaf0f7df2360f926b0c
rosen777 <rosen.toshev@flatironschool.com>
2018-12-12 14:43:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
+  const input = document.querySelector('body');
+
+input.addEventListener('keydown', testKonamiCode);
+}
+
+function testKonamiCode(e) {
   // your code here
+    const key = e.key;
+
+    if (key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+        alert('Easter egg.');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
 }

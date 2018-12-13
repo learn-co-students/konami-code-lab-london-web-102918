e1c9a1c232a4286bdf7734e207ef05f9896e50ab
artismarti <artismarti@gmail.com>
2018-12-13 09:58:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -26,17 +26,3 @@ function init() {
     }
   })
 }
-
-// let matches = 0
-// document.querySelector('body').addEventListener('keydown', function(event) {
-//   const userKey = event.key
-//   if (codes[matches] === userKey) {
-//     matches+= 1
-//   } else {
-//     matches = 0
-//   }
-//   if (matches === codes.length){
-//     alert("Yo")
-//     matches = 0
-//   }
-// })

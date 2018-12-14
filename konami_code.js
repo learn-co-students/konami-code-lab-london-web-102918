af1e989679955e99b5ffdcaf1a69c6517fabbff7
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-12-14 13:41:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+
 function init() {
+
   // your code here
-}
+  let index = 0;
+
+  const input = document.querySelector('body');
+
+  input.addEventListener('keydown', (check) => {
+    const key = check.key;
+
+    if (key === codes[index]) {
+      index++;
+
+    if (index === codes.length) {
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+
+})
+};

402c5fcdd85beea6c02bc0e8267fa512ac7c0a3d
Flatiron School <damanybailey@icloud.com>
2018-12-15 11:14:25 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
+let index = 0
+
 function init() {
   // your code here
+  document.body.addEventListener("keydown", (d) => {
+       const key = d.key;
+    if (key === codes[index]){
+      index++;
+
+      if (index === codes.length){
+        window.alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  } ) 
 }
